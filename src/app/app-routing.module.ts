18fe45d3dacbd5f0cea66837a870f92b421aaf15
Nassreddine Elmorched <elmorched.91n@gmail.com>
2025-08-03 15:44:54 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+import { LoginComponent } from './pages/login/login.component';
+import { IndexComponent } from './pages/index/index.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' }, // Redirect empty path to index
+  { path: 'index', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/index' }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
